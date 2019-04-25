9c4f40007b6dfb5d42f1dafb88c8f4c6ec69e8ab
Rachel <williamsrachelada@gmail.com>
2019-04-25 14:41:36 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(name)
+    add_song(Song.new(name))
+  end
+
+  def self.song_count
+    Song.all.length
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+
+  def add_post_by_title(title)
+    add_post(Post.new(title))
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+
+end
